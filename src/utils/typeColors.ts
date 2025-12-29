cc2b16b61686bdb6c8d2af95517c7406a84304e1
dante <dante.tarragau@gmail.com>
2025-12-28 19:59:04 -0500

    feat: add typeColors utility for Pokemon types with color configurations

---
+++ src/utils/typeColors.ts
@@ -0,0 +1,114 @@
+export interface TypeColorConfig {
+  bg: string
+  border: string
+  text: string
+}
+
+export const typeColorsMap: Record<string, TypeColorConfig> = {
+  normal: {
+    bg: 'bg-gray-400',
+    border: 'border-gray-500',
+    text: 'text-white'
+  },
+  fighting: {
+    bg: 'bg-red-600',
+    border: 'border-red-700',
+    text: 'text-white'
+  },
+  flying: {
+    bg: 'bg-indigo-400',
+    border: 'border-indigo-500',
+    text: 'text-white'
+  },
+  poison: {
+    bg: 'bg-purple-500',
+    border: 'border-purple-600',
+    text: 'text-white'
+  },
+  ground: {
+    bg: 'bg-amber-500',
+    border: 'border-amber-600',
+    text: 'text-white'
+  },
+  rock: {
+    bg: 'bg-amber-700',
+    border: 'border-amber-800',
+    text: 'text-white'
+  },
+  bug: {
+    bg: 'bg-lime-500',
+    border: 'border-lime-600',
+    text: 'text-white'
+  },
+  ghost: {
+    bg: 'bg-purple-600',
+    border: 'border-purple-700',
+    text: 'text-white'
+  },
+  steel: {
+    bg: 'bg-gray-400',
+    border: 'border-gray-500',
+    text: 'text-white'
+  },
+  fire: {
+    bg: 'bg-orange-500',
+    border: 'border-orange-600',
+    text: 'text-white'
+  },
+  water: {
+    bg: 'bg-blue-500',
+    border: 'border-blue-600',
+    text: 'text-white'
+  },
+  grass: {
+    bg: 'bg-green-500',
+    border: 'border-green-600',
+    text: 'text-white'
+  },
+  electric: {
+    bg: 'bg-yellow-400',
+    border: 'border-yellow-500',
+    text: 'text-black'
+  },
+  psychic: {
+    bg: 'bg-pink-500',
+    border: 'border-pink-600',
+    text: 'text-white'
+  },
+  ice: {
+    bg: 'bg-cyan-300',
+    border: 'border-cyan-400',
+    text: 'text-black'
+  },
+  dragon: {
+    bg: 'bg-indigo-600',
+    border: 'border-indigo-700',
+    text: 'text-white'
+  },
+  dark: {
+    bg: 'bg-gray-700',
+    border: 'border-gray-800',
+    text: 'text-white'
+  },
+  fairy: {
+    bg: 'bg-pink-300',
+    border: 'border-pink-400',
+    text: 'text-white'
+  },
+  stellar: {
+    bg: 'bg-indigo-500',
+    border: 'border-indigo-600',
+    text: 'text-white'
+  },
+  unknown: {
+    bg: 'bg-teal-500',
+    border: 'border-teal-600',
+    text: 'text-white'
+  },
+  shadow: {
+    bg: 'bg-gray-600',
+    border: 'border-gray-700',
+    text: 'text-white'
+  }
+}
+
