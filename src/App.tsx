affa59c103eda11f088970e1ba5f6e4384c67782
dante <dante.tarragau@gmail.com>
2025-12-28 19:58:49 -0500

    feat: implement Pokemon list with pagination controls and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { Suspense, useState } from 'react'
 
+import { getPokemons } from './services/pokemonService'
+import { PokemonList } from './components'
 
 function App() {
+  const [limit, setLimit] = useState(20)
+  const [offset, setOffset] = useState(0)
+
+  const handleLimitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLimit(parseInt(event.target.value))
+  }
+
+  const handleOffsetChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOffset(parseInt(event.target.value))
+  }
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PokemonList pokemonListPromise={getPokemons(limit, offset)} />
+    </Suspense>
   )
 }
 
